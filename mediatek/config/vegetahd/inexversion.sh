a5bfcd2f71432f795bc0a7c8073944b1a7f52f31
John McAleely <john.mcaleely@canonical.com>
2015-05-01 09:37:09 -0500

    build: set CKT version prefix for VegetaHD
    Project prefix:
    
    VEGETA01A-S11A_BQ
    
    Change-Id: I76b77f381aadedaf20b79e7af5dce3bf1f315476

--- mediatek/config/vegetahd/inexversion.sh
+++ mediatek/config/vegetahd/inexversion.sh
@@ -28,7 +28,7 @@ production_channel="ubuntu-touch/stable/bq-aquaris.en"
 external_version=""
 internal_version=""
 
-projectandoem="KRILIN01A-S15A_BQ"
+projectandoem="VEGETA01A-S11A_BQ"
 current_langs="100"
 
 # fake a build time for the internal version. first build of the day
